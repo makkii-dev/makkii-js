41ebccfa7e21ae709e2d6194985b3066e83d09de
chen <chen@aion.network>
2019-05-22 12:51:26 +0800

    update ios lib

--- ios/RNMakkiiCore.m
+++ ios/RNMakkiiCore.m
@@ -2,6 +2,10 @@
 
 @interface RCT_EXTERN_MODULE(RNMakkiiCore, NSObject)
 RCT_EXTERN_METHOD(createByMnemonic:(NSString *)mnemonic passphrase:(NSString *)passphrase)
-RCT_EXTERN_METHOD(generateMnemonic:(RCTPromiseResolveBlock) resolve rejecter:(RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(generateMnemonic:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(getKey:(nonnull NSNumber *)coinType account:(nonnull NSNumber *)path1 change:(nonnull NSNumber *)path2 address:(nonnull NSNumber *)path3 resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(recoverKeyPairByPrivateKey:(NSString*)privatekey coinType:(nonnull NSNumber *)coin reslover:(RCTPromiseResolveBlock)reslove rejecter:(RCTPromiseReject)reject)
+RCT_EXTERN_METHOD(sign:(NSString *)hash privatekey:(NSString *)privateKey coinType:(nonnull NSNumber *)coin reslover:(RCTPromiseResolveBlock)reslove rejecter:(RCTPromiseReject)reject)
+RCT_EXTERN_METHOD(signTransaction:(NSDictionary *)transaction privatekey:(NSString *)privateKey coinType:(nonnull NSNumber *)coin reslover:(RCTPromiseResolveBlock)reslove rejecter:(RCTPromiseReject)reject)
+
 @end
--- ios/RNMakkiiCore.swift
+++ ios/RNMakkiiCore.swift
@@ -13,6 +13,8 @@ import TrustWalletCore
     enum BaseError:Error {
         case InvalidCoinType
         case InvalidDerivePath
+        case InvalidPrivateKey
+        case InvalidDigestData
         case UnknowReason
     }
     @objc func generateMnemonic(_ resolve:RCTPromiseResolveBlock,rejecter reject:RCTPromiseRejectBlock){
@@ -49,6 +51,99 @@ import TrustWalletCore
         }
     }
     
+    @objc func recoverKeyPairByPrivateKey(_ privateKey: String, cointType coin: UInt32 , resolver resolve: RCTPromiseResolveBlock, rejecter reject:RCTPromiseRejectBlock) {
+        do{
+            guard let cointype = getCoinType(coin) else {
+                throw BaseError.InvalidCoinType
+            }
+            guard let sk = getPrivateKeyByString(privateKey) else {
+                throw BaseError.InvalidPrivateKey
+            }
+            guard let pk = getPublicKeyByCoin(private_key: sk, coinType: cointype) else {
+                throw BaseError.UnknowReason
+            }
+            var skstr = sk.data.hexString
+            if cointype == CoinType.aion {
+                skstr = skstr + pk.data.hexString
+            }
+            resolve(["private_key":skstr, "public":pk.data.hexString, "address": cointype.deriveAddress(privateKey: sk)])
+            
+        }catch BaseError.InvalidCoinType {
+            reject("E_INVALID_COINTYPE" ,"invalid coinType", nil)
+        }catch BaseError.InvalidPrivateKey {
+            reject("E_INVALID_PrivateKey" ,"invalid privateKey", nil)
+        }catch {
+            reject("E_UNKNOWN_REASON", "unknown reason", nil)
+        }
+    }
+    
+    @objc func sign(_ hash:String, privateKey private_key:String, coinType coin:UInt32, resolver resolve:RCTPromiseResolveBlock, rejecter reject:RCTPromiseRejectBlock) {
+        do{
+            guard let cointype = getCoinType(coin) else {
+                throw BaseError.InvalidCoinType
+            }
+            guard let sk = getPrivateKeyByString(private_key) else {
+                throw BaseError.InvalidPrivateKey
+            }
+            guard let hashdata = Data(hexString: hash) else {
+                throw BaseError.InvalidDigestData
+            }
+            let signature = sk.sign(digest: hashdata, curve: cointype.curve)!
+            resolve(signature.hexString)
+        }catch BaseError.InvalidCoinType {
+            reject("E_INVALID_COINTYPE" ,"invalid coinType", nil)
+        }catch BaseError.InvalidPrivateKey {
+            reject("E_INVALID_PrivateKey" ,"invalid privateKey", nil)
+        }catch BaseError.InvalidDigestData {
+            reject("E_INVALID_Digest" ,"invalid digest data", nil)
+        }catch {
+            reject("E_UNKNOWN_REASON", "unknown reason", nil)
+        }
+    }
+    
+    @objc func signTransaction(_ transaction:[String:Any], coinType coin:UInt32, resolver resolve:RCTPromiseResolveBlock, rejecter reject:RCTPromiseRejectBlock){
+        if coin == CoinType.aion.rawValue {
+            var pkstr: String = transaction["private_key"] as! String;
+            if pkstr.starts(with: "0x") {
+                pkstr = String(pkstr[pkstr.index(pkstr.startIndex, offsetBy: 2)..<pkstr.endIndex])
+            }
+            if pkstr.count>64 {
+                pkstr = String(pkstr[..<pkstr.index(pkstr.startIndex, offsetBy: 64)])
+            }
+            let input = AionSigningInput.with{
+                $0.nonce = Data(hexString: transaction["nonce"] as! String)!
+                $0.gasPrice = Data(hexString: transaction["gasPrice"] as! String)!
+                $0.gasLimit = Data(hexString: transaction["gasLimit"] as! String)!
+                $0.toAddress = transaction["to"] as! String
+                $0.amount = Data(hexString: transaction["amount"] as! String)!
+                $0.timestamp = Data(hexString: transaction["timestamp"] as! String)!
+                $0.privateKey = Data(hexString: pkstr)!
+            }
+            let output = AionSigner.sign(input: input)
+            resolve(["signature":output.signature.hexString, "encoded":output.encoded.hexString])
+        }else if coin == CoinType.bitcoin.rawValue || coin == CoinType.litecoin.rawValue {
+            
+        }else if coin == CoinType.eos.rawValue {
+            
+        }else if coin == CoinType.ethereum.rawValue {
+            let input = TW_Ethereum_Proto_SigningInput.with{
+                $0.chainID = Data(hexString: transaction["chainID"] as! String)!
+                $0.nonce = Data(hexString: transaction["nonce"] as! String)!
+                $0.gasPrice = Data(hexString: transaction["gasPrice"] as! String)!
+                $0.gasLimit = Data(hexString: transaction["gasLimit"] as! String)!
+                $0.toAddress = transaction["to"] as! String
+                $0.amount = Data(hexString: transaction["amount"] as! String)!
+                $0.privateKey = Data(hexString: transaction["private_key"] as! String)!
+            }
+            let output = EthereumSigner.sign(input: input)
+            resolve(["encoded":output.encoded.hexString, "v":output.v.hexString, "r":output.r.hexString,"s":output.s.hexString])
+        }else if coin == CoinType.tron.rawValue {
+            
+        }else {
+            reject("E_INVALID_COINTYPE" ,"invalid coinType", nil)
+        }
+    }
+    
     func getCoinType(_ coin:UInt32) ->CoinType?{
         switch coin {
         case CoinType.aion.rawValue:
@@ -79,5 +174,19 @@ import TrustWalletCore
         default:
             return nil
         }
+    }
+    
+    func getPrivateKeyByString(_ str:String)->PrivateKey?{
+        var pkstr: String = str;
+        if pkstr.starts(with: "0x") {
+            pkstr = String(pkstr[pkstr.index(pkstr.startIndex, offsetBy: 2)..<pkstr.endIndex])
+        }
+        if pkstr.count>64 {
+            pkstr = String(pkstr[..<pkstr.index(pkstr.startIndex, offsetBy: 64)])
+        }
+        guard let sk = PrivateKey(data: Data(hexString: pkstr)!) else {
+            return nil
+        }
+        return sk
     }
 }
