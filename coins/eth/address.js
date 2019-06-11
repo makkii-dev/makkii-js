c297822669ff21362cdcc989524f4dbec5904c74
Chengchcc <chen@aion.network>
2019-06-11 16:03:43 +0800

    update eth validateAddress

--- coins/eth/address.js
+++ coins/eth/address.js
@@ -1,5 +1,5 @@
-import {isValidChecksumAddress} from 'ethereumjs-util';
+import {isValidAddress} from 'ethereumjs-util';
 
 export const validateAddress = (address) => new Promise((resolve, reject) => {
-   resolve(isValidChecksumAddress(address))
+   resolve(isValidAddress(address))
 });
