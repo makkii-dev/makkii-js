1da406e113f69f114344369a52539339c19a77d1
Chengchcc <chen@aion.network>
2019-07-30 15:01:37 +0800

    export api_caller as httpclient

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 import { CoinType } from './coins/coinType'
 import {client as keystoreClient} from './keystore';
 import {client as apiClient} from "./api";
-
+import HttpClient from  './utils/Api_caller';
 export {
     keystoreClient,
     apiClient,
+    HttpClient,
     CoinType
 };
