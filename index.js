9134f97b9d6c74d5d6d511fb4c4b9d63726aafb2
Chengchcc <chen@aion.network>
2019-07-31 12:48:26 +0800

    export setCurrentServer

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import { CoinType } from './coins/coinType'
 import {client as keystoreClient} from './keystore';
 import {client as apiClient} from "./api";
 import HttpClient from  './utils/Api_caller';
+import {setCurrentServer} from "./remote_server";
+
 export {
     keystoreClient,
     apiClient,
     HttpClient,
-    CoinType
+    CoinType,
+    setCurrentServer
 };
--- remote_server.js
+++ remote_server.js
@@ -16,25 +16,32 @@ const prod = {
 let app_server_static = qa.app_server_static;
 let app_server_api = qa.app_server_api;
 
-const setCurrentSever=(server)=>{
-    switch (server) {
-        case 'qa':
-            app_server_static = qa.app_server_static;
-            app_server_api = qa.app_server_api;
-            break;
-        case 'prod':
-            app_server_static = prod.app_server_static;
-            app_server_api = prod.app_server_api;
-            break;
-        default:
-            app_server_static = staging.app_server_static;
-            app_server_api = staging.app_server_api;
+const setCurrentServer=(server)=>{
+    if(typeof  server === 'string') {
+        switch (server) {
+            case 'qa':
+                app_server_static = qa.app_server_static;
+                app_server_api = qa.app_server_api;
+                break;
+            case 'prod':
+                app_server_static = prod.app_server_static;
+                app_server_api = prod.app_server_api;
+                break;
+            default:
+                app_server_static = staging.app_server_static;
+                app_server_api = staging.app_server_api;
+        }
+    }else if(typeof  server === 'object'){
+        app_server_static = server.app_server_static;
+        app_server_api = server.app_server_api;
+    }else{
+        throw "Parse error"
     }
 };
 
 export {
     app_server_api,
     app_server_static,
-    setCurrentSever
+    setCurrentServer
 }
 
