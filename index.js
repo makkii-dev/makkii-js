3bcf39d0f642272b86fbd52e0b5975ae46b94e59
Leo Ren <leo.ren@aion.network>
2019-08-08 14:10:19 +0800

    remove http client export

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 import { CoinType } from './coins/coinType'
 import {client as keystoreClient} from './keystore';
 import {client as apiClient} from "./api";
-import HttpClient from  './utils/Api_caller';
 import {setCurrentServer} from "./remote_server";
 
 export {
     keystoreClient,
     apiClient,
-    HttpClient,
     CoinType,
     setCurrentServer
 };
