80f99c3ca1b6dbb3be5f6a927d4372be32794671
Chengchcc <chen@aion.network>
2019-11-18 18:33:30 +0800

    remove lib

--- packages/app-eth/lib/keystore/address.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ethereumjs_util_1 = require("ethereumjs-util");
-exports.validateAddress = (address) => new Promise((resolve, reject) => {
-    resolve(ethereumjs_util_1.isValidAddress(address));
-});
-//# sourceMappingURL=address.js.map
--- packages/app-tron/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var apiClient_1 = require("./apiClient");
-exports.TronApiClient = apiClient_1.default;
-var keystoreClient_1 = require("./keystoreClient");
-exports.TronKeystoreClient = keystoreClient_1.default;
-//# sourceMappingURL=index.js.map
