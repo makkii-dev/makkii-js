12b2d520e093da28b611233089de72043b516bb3
Chengchcc <chen@aion.network>
2019-12-03 12:12:20 +0800

    upgrade aion-web3-eth-abi aion-web3-eth-contract

--- packages/app-aion/lib/lib_keystore/address.js
+++ packages/app-aion/lib/lib_keystore/address.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.inputAddressFormatter = (address) => {
-    if (/^0x[A|a]0[0-9a-f]{62}$/i.test(address)) {
+    if (/^(0x)?[A|a]0[0-9a-f]{62}$/i.test(address)) {
         return `0x${address.toLowerCase().replace('0x', '')}`;
     }
     throw new Error(`Provided address "${address}" is invalid`);
