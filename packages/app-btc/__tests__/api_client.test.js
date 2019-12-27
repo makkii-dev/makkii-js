0772f8796ba8e1fb3f7d27c9b4ec928e206fad38
Chengchcc <chen@aion.network>
2019-12-27 16:46:22 +0800

    update btc test api

--- packages/app-btc/__tests__/api_client.test.js
+++ packages/app-btc/__tests__/api_client.test.js
@@ -6,7 +6,7 @@ const { BtcApiClient, BtcLocalSigner } = require('../lib/index');
 
 const client = new BtcApiClient({
     network: 'BTCTEST',
-    insight_api: 'http://45.118.132.89:3001/api'
+    insight_api: 'http://47.92.202.146/:3001/api'
 });
 
 const testAddress = 'mn7bpjgaPdAs2i9GuT8i3bcyreiFZ2ZXmN';
